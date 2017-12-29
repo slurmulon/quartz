5ae7dd488158f74d8916a5747db28f940545c33e
evavro <me@madhax.io>
2017-12-28 20:00:44 -0800

    :lock: :sparkles: improved type declarations for web-audio-scheduler

--- types/web-audio-scheduler/index.d.ts
+++ types/web-audio-scheduler/index.d.ts
@@ -1,16 +1,49 @@
 declare module 'web-audio-scheduler' {
 
-  class WebAudioScheduler {
-    context: Object
+  interface SchedulerOptions {
+    context?: AudioContext | Object
+    interval?: number
+    aheadTime?: number
+    timerAPI?: Timer
+  }
+
+  class WebAudioScheduler implements SchedulerOptions {
+    context: AudioContext | Object
     interval: number
     aheadTime: number
-    currentTime: number
     timerAPI: Timer
+    _timerId: number
+    _schedId: number
+    _scheds: Array<Object>
+
+    constructor (opts: SchedulerOptions)
+
+    state (): string
+    currentTime (): number
+    events (): Array<Event>
+
+    start (callback: Function, ...args: any[]): this
+    stop (reset: boolean): WebAudioScheduler
+    insert (time: number, callback: Function, ...args: any[]): number
+    nextTick (time: number, callback: Function, ...args: any[]): number
+    remove (schedId: number): number
+    removeAll (): void
+    process (t0: number, t1: number): void
+
+  }
+
+  interface Event {
+    id: number
+    time: number
+    callback: Function
+    args: Array<any>
   }
 
   interface Timer {
     setInterval (func: Function, wait: number): Object
     clearInterval (interval: number): void
   }
+
+  export = WebAudioScheduler
 
 }
